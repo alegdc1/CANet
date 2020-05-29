cd6e1669125752279ea40d4719660c9c6f6fe59d
Alejandro Garcia-Rodrigues <garciaal@petzi.ee.ethz.ch>
2020-05-29 20:28:50 +0200

    setup sh files

---
+++ messidor_scripts/train.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#$ -o qsub_output
+#$ -S /bin/bash
+#$ -j y
+#$ -cwd
+#$ -l gpu=1
+#$ -l h_vmem=40G
+source /itet-stor/garciaal/net_scratch/conda/etc/profile.d/conda.sh
+
+# From here, it's just what you executed in qrsh
+conda activate cutmix
+# ... maybe other stuff here ...
+ 
+# Now, you should use -u to get unbuffered output and "$@" for any arguments
+sh train_fold.sh

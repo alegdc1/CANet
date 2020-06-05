bea782ae08b7b9b0600225f2c216898185cd3a5b
Alejandro Garcia-Rodrigues <garciaal@petzi.ee.ethz.ch>
2020-06-05 09:22:49 +0200

    -Moved the results file to the exp folder. -Created an evaluation script to run eval_fold. - Updated a few things like Epoch size to run experiments faster. - Made a save chechpoint in baseline to save the model in the training process. -There are a few results in this commit

---
+++ messidor_scripts/eval.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#$ -o qsub_output
+#$ -S /bin/bash
+#$ -j y
+#$ -cwd
+#$ -l gpu=1
+#$ -l h_vmem=80G
+source /itet-stor/garciaal/net_scratch/conda/etc/profile.d/conda.sh
+
+# From here, it's just what you executed in qrsh
+conda activate cutmix
+# ... maybe other stuff here ...
+ 
+# Now, you should use -u to get unbuffered output and "$@" for any arguments
+sh eval_fold.sh
+
--- messidor_scripts/train_fold.sh
+++ messidor_scripts/train_fold.sh
@@ -6,6 +6,6 @@ do
     FILE="${var}/multi_CAN_lamda25_noshare_wpre_simpleaug_10fold$NUM""_1000"
     FOLD="${var}fold$NUM"
     python baseline.py ./data/ missidor exp/MESSIDOR/$FILE --fold_name $FOLD -a resnet50 \
-    --gpu 0 -b 8 --base_lr 3e-4 --pretrained --epochs 300  --decay_epoch 150 --num_class 2 --adam --multitask \
+    --gpu 0 -b 8 --base_lr 3e-4 --pretrained --epochs 2  --decay_epoch 1 --num_class 2 --adam --multitask \
     --crossCBAM --choice both --lambda_value 0.25
 done

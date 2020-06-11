682332519fe6010b9d623b44055bb89fecc5f9bc
alegdc1 <garciaal@student.ethz.ch>
2020-06-11 11:42:35 +0200

    -baseline: save checkpoint (torch load)/ -eval fold gpu 0 - baseline debug code with numbers to check where it stops working

--- messidor_scripts/eval_fold.sh
+++ messidor_scripts/eval_fold.sh
@@ -5,6 +5,6 @@ do
     NUM="${var}$i"
     FILE="${var}/multi_CAN_lamda25_noshare_wpre_simpleaug_10fold$NUM""_1000"
     FOLD="${var}fold$NUM"
-    python baseline.py /raid/li/datasets/MESSIDOR/ missidor exp/MESSIDOR/$FILE --fold_name $FOLD -a resnet50 --gpu 8 -b 8 --base_lr 3e-4 --pretrained --epochs 20  --decay_epoch 10  --multitask --crossCBAM --choice both --adam --evaluate --resume exp/MESSIDOR/$FILE/model_converge.pth.tar
+    python baseline.py /raid/li/datasets/MESSIDOR/ missidor exp/MESSIDOR/$FILE --fold_name $FOLD -a resnet50 --gpu 0 -b 8 --base_lr 3e-4 --pretrained --epochs 20  --decay_epoch 10  --multitask --crossCBAM --choice both --adam --evaluate --resume exp/MESSIDOR/$FILE/model_converge.pth.tar
 done
 python average_result.py --filename multi_CAN_lamda25_noshare_wpre_simpleaug_10fold

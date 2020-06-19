05df5dd557dd23d4e4a7c3ceb4001303e1f9c2c8
alegdc1 <garciaal@student.ethz.ch>
2020-06-19 10:49:53 +0200

    Adaptations in ODIR.py

--- datasets/ODIR.py
+++ datasets/ODIR.py
@@ -37,12 +37,15 @@ class traindataset(data.Dataset):
 
         # todo generate file_list of all left/right images in dataset
         files = np.loadtxt(self.root + "file_list.txt", dtype=str)
+        # only take first 100 train images
+        files = files[0:100]
 
         # todo generate the folds
         # idx = np.loadtxt(self.root + "/10fold/" + str(args.fold_name) + ".txt", dtype=int)
 
         # test only with 3 images for now
-        idx = np.random.choice(range(7000), 3, replace=False)
+        #idx = np.random.choice(range(7000), 3, replace=False)
+        idx = [0,1,2]
 
         self.test_root = [files[idx_item] for idx_item in idx]
         self.train_root = list(set(files) - set(self.test_root))
@@ -58,8 +61,7 @@ class traindataset(data.Dataset):
         if self.mode == 'train':
             # self.train_root = [item.replace("/medical/", "/Documents/datasets/medical/") for item in self.train_root]
             for each_one in self.train_root:
-                img = Image.open(each_one)
-                img = img.convert('RGB')
+                img_file = each_one.split("/")[-1]
 
                 # skip bad images, like "no fundus image" or "low img quality"
                 if each_one in self.ignored_images:
@@ -67,15 +69,15 @@ class traindataset(data.Dataset):
 
                 # create network input (train data, label)
                 # dictLabels = {"N" : ['/data/1_right.tif', '']}
-                labels = [k for k, v in dictLabels.items() if each_one.split("/")[-1] in v]
+                labels = [k for k, v in dictLabels.items() if img_file in v]
 
                 # labels = {"G", "C", "A"}
                 label_vector = self.to_one_hot_vector(labels)
 
                 self.train_label.append(label_vector)
 
-                self.train_data.append(img)
-                self.name.append(each_one.split("/")[-1])
+                self.train_data.append(img_file)
+                self.name.append(img_file)
             assert len(self.train_label) == len(self.train_data)
 
             print('=> Total Train: ', len(self.train_data), " ODIR images ")
@@ -85,22 +87,22 @@ class traindataset(data.Dataset):
             # self.test_root = [item.replace("/medical/", "/Documents/datasets/medical/") for item in self.test_root]
 
             for item in self.test_root:
-                img = Image.open(item)
-                img = img.convert('RGB')
+                img_file = item.split("/")[-1]
 
                 if item in self.ignored_images:
                     continue
 
                 # dictLabels = {"N" : ['/data/1_right.tif', '']}
-                labels = [k for k, v in dictLabels.items() if item.split("/")[-1] in v]
+                labels = [k for k, v in dictLabels.items() if img_file in v]
 
                 # labels = {"G", "C", "A"}
                 label_vector = self.to_one_hot_vector(labels)
 
                 self.test_label.append(label_vector)
 
-                self.test_data.append(img)
-                self.name.append(item.split("/")[-1])
+                #TODO: instead of appending image, append file name
+                self.test_data.append(img_file)
+                self.name.append(img_file)
             assert len(self.test_data) == len(self.test_label)
 
             print('=> Total Test: ', len(self.test_data), " ODIR images ")
@@ -239,9 +241,12 @@ class traindataset(data.Dataset):
         """
 
         if self.mode == 'train':
-            img, label, name = self.train_data[index], self.train_label[index], self.name[index]
+            img_file, label, name = self.train_data[index], self.train_label[index], self.name[index]
         elif self.mode == 'val':
-            img, label, name = self.test_data[index], self.test_label[index], self.name[index]
+            img_file, label, name = self.test_data[index], self.test_label[index], self.name[index]
+
+        img = Image.open(img_file)
+        img = img.convert('RGB')
 
         img = self.transform(img)
 
